ebf6fd0e1d1e6b18b1bd866093458ce6fc3f0d9c
weisonweileen <weisonweileen@gmail.com>
2025-03-28 19:08:48 +0800

    FIX 2r robot
    Signed-off-by: weisonweileen <weisonweileen@gmail.com>

--- 2r_robot.py
+++ 2r_robot.py
@@ -6,18 +6,30 @@ import time
 
 m = mujoco.MjModel.from_xml_path("pemdulem.xml")
 m.opt.timestep = 0.01
-data = mujoco.MjData(m)
-# with mujoco.viewer.launch_passive(m, data) as viewer:
-viewer = mujoco.viewer.launch(m, data)
+d = mujoco.MjData(m)
+# with mujoco.viewer.launch_passive(m, d) as viewer:
+# viewer = mujoco.viewer.launch_passive(m, d)
 # while True:
 # start = time.time()
 step_count = 0
 # render_interval = 100
 # for i in range(13000):
-while True:
-    step_start = time.time()
-    print(data.qpos)
-    mujoco.mj_step(m, data)
-    # if step_count % render_interval == 0:
-    viewer.render()
-    step_count += 1
+# print(d.qpos)
+d.qpos[0] = - np.pi /2 + 0.02
+# print(d.qpos)
+step_interval = 10
+# print(d.qpos)
+# exit()
+
+with mujoco.viewer.launch_passive(m, d) as viewer:
+    while viewer.is_running():
+        step_start = time.time()
+        print(d.qpos)
+        # if step_count % step_interval == 0:
+        mujoco.mj_step(m, d)
+        viewer.sync()
+        # step_count += 1
+
+        time_until_next_step = m.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
